4bc8900d69508ec9dc40b5f157f4adc9a3971079
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-03-20 09:21:40 -0300

    add - questao 8

---
+++ exercicio_08.js
@@ -0,0 +1,9 @@
+function conta() {
+    let a = 10;
+    let b = 5;
+
+    console.log("Diferença: " + (a - b));
+    console.log("Dobro de A + Triplo de B: " + (a * 2) + (b * 3));
+    console.log("Multiplicação de A e B: " + (a * b));
+
+}
