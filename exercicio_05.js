5366f7b5198fc90863637bfe6888dde2e34f251b
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-03-20 09:20:52 -0300

    add - questao 5

---
+++ exercicio_05.js
@@ -0,0 +1,11 @@
+function parImp(){
+    let numero = Number(prompt("Digite um numero: "));
+
+    if (numero % 2 == 0) {
+        numero +- 1
+    }else{
+        numero +- 1
+    }
+
+    alert("Resultado: " + numero);
+}
