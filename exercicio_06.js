5174cfd9f7018b9fb3bb6201906a5cb8be551b28
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-03-20 09:21:04 -0300

    add - questao 6

---
+++ exercicio_06.js
@@ -0,0 +1,15 @@
+function vogalOuConsoante() {
+
+    let letra = prompt("Digite uma letra: ").toLowerCase();
+
+    if (
+        letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"
+    
+    ) {
+        console.log("Vogal");
+
+    } else {
+        console.log("Consoante");
+        
+    }
+}
