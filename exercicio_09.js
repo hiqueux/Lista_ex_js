d0a7e6348b32fc28817c1cf8046051ae9bfaee7b
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-03-20 09:21:49 -0300

    add - questao 9

---
+++ exercicio_09.js
@@ -0,0 +1,12 @@
+function ordenaNum() {
+    let a = Number(prompt("Digite o primeiro número: "));
+    let b = Number(prompt("Digite o segundo número: "));
+
+    if (a > b) {
+        console.log(a, b);
+
+    } else {
+        console.log(b, a);
+        
+    }
+}
